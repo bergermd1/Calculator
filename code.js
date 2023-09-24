b785004ac90d18a14e618923e99535cf0f68a1f5
Matt Berger <bergermd1@gmail.com>
2023-09-23 21:39:30 -0500

    Tentatively complete interface/calculator logic functionality

--- code.js
+++ code.js
@@ -1,36 +1,61 @@
-// const readline = require('readline').createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   });
-const prompt = require('prompt-sync')();
+// const prompt = require('prompt-sync')();
 
 let inputs = [];
 let operators = [`+`, `-`, `*`, `/`];
 let numbers = [`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,`0`];
-let functions = [`c`, `e`];
+let functions = [`e`, `c`];
 let validInputs = operators.concat(numbers, functions);
 let keepGoing = true;
+let display = ``;
+let displayDiv;
+let nonNumberButtons = operators.concat(functions);
+window.onload = (event) => {
+    console.log('yeah');
+    for (let i = 0; i <= 9; i++) {
+        let btn = document.querySelector(`.b${i}`)
+        console.log(btn);
+        btn.addEventListener(`click`, () => {
+            collectInput(i.toString());
+        })
+    }
+    for (let i = 0; i < 6; i++) {
+        let btn = document.querySelector(`.b1${i}`)
+        // console.log(btn);
+        // console.log(nonNumberButtons[i].toString());
+        btn.addEventListener(`click`, () => {
+            collectInput(nonNumberButtons[i].toString())
+        })
+    }
+    displayDiv = document.querySelector(`.rect`);
+}
 
-while (keepGoing) {
-    let input = getInput();
+function collectInput(input) {
+    // let input = getInput();
     if (input != undefined) {
         //don't add operator if operator was most recent input
         if (numbers.includes(input)) {
             inputs.push(input);
+            display += input;
         } else if (operators.includes(input)) {
             if (numbers.includes(inputs[inputs.length - 1])) {
                 inputs.push(input);
+                display += input;
             }
         } else if (functions.includes(input)) {
             if (input === `c`) {
                 inputs = [];
+                display = ``;
             } else if (input === `e` && numbers.includes(inputs[inputs.length - 1])) {
                 //only use e (enter) after a number has been inputted 
-                keepGoing = false;
-                console.log(processInputs());
+                // keepGoing = false;
+                console.log(inputs);
+                display = processInputs();
             }
         }
     }
+    displayDiv.textContent = display;
+    console.log(inputs);
+    // console.log(display);
 }
 
 function getInput() {
@@ -66,6 +91,9 @@ function evaluateInputs(processedInputs) {
             processedInputs = processedInputs.slice(3);
             processedInputs.unshift(result);
     }
+    // console.log(typeof processedInputs[0]);
+    inputs = processedInputs[0].toString().split("");/////
+    console.log(inputs);
     return processedInputs[0]
 }
 
